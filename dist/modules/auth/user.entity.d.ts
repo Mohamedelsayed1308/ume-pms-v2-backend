014198da70cdf0a65a228990624f748b9aeab757
Mohamed Elsayed <mezzoabd@gmail.com>
2026-07-17 12:25:15 +0300

    remove sensitive and heavy files

--- dist/modules/auth/user.entity.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class User {
-    id: string;
-    email: string;
-    password: string;
-    full_name: string;
-    role: string;
-    is_active: boolean;
-    created_at: Date;
-}
--- dist/modules/currencies/currency.entity.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class Currency {
-    id: string;
-    code: string;
-    name: string;
-    rate_to_usd: number;
-    is_active: boolean;
-    created_at: Date;
-}
